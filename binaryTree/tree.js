df88903e271446e4b7dd94bc040f55630a44fc83
exp1 <gorvkrtianliya@gmail.com>
2023-03-18 12:20:56 +0530

    completed all func

--- binaryTree/tree.js
+++ binaryTree/tree.js
@@ -2,45 +2,47 @@ import { Node } from "./node";
 import { prettyPrint } from "./helpFunction";
 import { alreadyExist, doNotExist } from "./error";
 
-const Tree = (rawArr) => {
+export const buildTree = (arrr, start = 0, end = arrr.length - 1) => {
+  if (start > end) return null;
+  const mid = Math.round((start + end) / 2);
+
+  const root = Node(arrr[mid]);
+
+  root.left = buildTree(arrr, start, mid - 1);
+  root.right = buildTree(arrr, mid + 1, end);
+
+  return root;
+};
+
+export const Tree = (rawArr) => {
   let treeRoot;
-  rawArr = [...new Set(rawArr)];
+  const Arr = [...new Set(rawArr)];
 
-  const buildTree = (arrr, start, end) => {
-    if (start > end) return null;
-    const mid = Math.round((start + end) / 2);
+  treeRoot = buildTree(Arr);
 
-    const root = Node(arrr[mid]);
-
-    root.left = buildTree(arrr, start, mid - 1);
-    root.right = buildTree(arrr, mid + 1, end);
-
-    return root;
-  };
-  treeRoot = buildTree(rawArr, 0, rawArr.length - 1);
-
-  const find = (value, binTree = treeRoot) => {
+  function find(value, binTree = treeRoot) {
     let node = binTree;
 
     while (node !== null) {
       if (node.data === value) {
         return node;
-      } else if (value < node.data) {
+      }
+      if (value < node.data) {
         node = node.left;
       } else {
         node = node.right;
       }
     }
     return null;
-  };
+  }
 
-  const insert = (value, binTree = treeRoot) => {
+  function insert(value, binTree = treeRoot) {
     if (find(value)) {
       alreadyExist(value);
       return;
     }
 
-    let node = binTree;
+    const node = binTree;
     const root = Node(value);
 
     if (value < node.data) {
@@ -58,112 +60,169 @@ const Tree = (rawArr) => {
 
       insert(value, node.right);
     }
-  };
+  }
 
-  const searchLeftMostNode = (node) => {
+  function findNextBiggestNode(node) {
     let root = node;
 
     if (root.left === null) return root;
-    root = searchLeftMostNode(root.left);
+    root = findNextBiggestNode(root.left);
 
     return root;
-  };
+  }
 
-  const searchParentNode = (value) => {
+
+  function deleteNodeRec(tree, value) {
+    if (tree == null) return tree;
+
+    if (value < tree.data) {
+      tree.left = deleteNodeRec(tree.left, value);
+    } else if (value > tree.data) {
+      tree.right = deleteNodeRec(tree.right, value);
+    } else {
+      if (tree.left == null) {
+        return tree.right;
+      }
+      if (tree.right == null) {
+        return tree.left;
+      }
+      tree.data = findNextBiggestNode(tree.right).data;
+      tree.right = deleteNodeRec(tree.right, tree.data);
+    }
+
+    return tree;
+  }
+
+  function deleteNode(nodeValue) {
+    treeRoot = deleteNodeRec(treeRoot, nodeValue);
+  }
+
+  function depthSearchRec(node, func, queue = [treeRoot]) {
+    if (node == null) return;
+
+    func(node.data);
+    if (node.left !== null) queue.push(node.left);
+    if (node.right !== null) queue.push(node.right);
+    queue.shift();
+
+    depthSearchRec(queue[0], func, queue);
+  }
+
+  function levelOrder(func) {
+    depthSearchRec(treeRoot, func);
+  }
+
+  function inorder(func, node = treeRoot) {
+    if (node == null) return;
+
+    inorder(func, node.left);
+    func(node.data);
+    inorder(func, node.right);
+  }
+
+  function postorder(func, node = treeRoot) {
+    if (node == null) return;
+
+    postorder(func, node.left);
+    postorder(func, node.right);
+    func(node.data);
+  }
+
+  function preorder(func, node = treeRoot) {
+    if (node == null) return;
+
+    func(node.data);
+    preorder(func, node.left);
+    preorder(func, node.right);
+  }
+
+  function heightRec(node) {
+    if (node == null) {
+      return 0;
+    }
+    const leftHeight = heightRec(node.left);
+    const rightHeight = heightRec(node.right);
+
+    if (leftHeight < rightHeight) return rightHeight + 1;
+
+    return leftHeight + 1;
+  }
+
+  function height(nodeValue) {
+    const node = find(nodeValue);
+    if (node == null) return doNotExist(nodeValue);
+    return heightRec(node);
+  }
+
+  function depth(nodeValue) {
     let node = treeRoot;
-    let parentNode;
-
-    while (node.data !== value) {
-      parentNode = node;
-      if (value < node.data) {
+    let num = 0;
+    while (node !== null) {
+      if (node.data === nodeValue) {
+        return num;
+      }
+      if (nodeValue < node.data) {
         node = node.left;
+        num += 1;
       } else {
         node = node.right;
+        num += 1;
       }
     }
-    return parentNode;
-  };
+    return doNotExist(nodeValue);
+  }
 
-  // const deleteNode = (value, binTree = treeRoot) => {
-  //   let node = treeRoot;
-  //   if (!find(value)) {
-  //     doNotExist(value);
-  //     return;
-  //   }
-  //   let parentNode;
-  //   let grandChildNode;
-  //   let pos;
-  //   while (node.data !== value) {
-  //     parentNode = node;
-  //     if (value < node.data) {
-  //       node = node.left;
-  //       pos = "left";
-  //       grandChildNode = node.left;
-  //     } else {
-  //       node = node.right;
-  //       pos = "right";
-  //       grandChildNode = node.right;
-  //     }
-  //   }
-  //   // console.log(parentNode);
+  function isBalancedRec(node = treeRoot) {
+    if (node == null) return;
+    const leftNode = node.left;
+    const rightNode = node.right;
 
-  //   if (node.left === null && node.right === null) {
-  //     parentNode[pos] = null;
-  //   }
-  //   if (
-  //     (node.left === null && node.right !== null) ||
-  //     (node.right === null && node.left !== null)
-  //   ) {
-  //     console.log(node);
-  //     if (node.left !== null) {
-  //       parentNode["left"] = node.left;
-  //     } else {
-  //       parentNode["right"] = node.right;
-  //     }
-  //   }
-  //   if (node.right !== null && node.left !== null) {
+    if (leftNode == null || rightNode == null) return;
 
-  //     grandChildNode = searchLeftMostNode(node.right);
-  //     const pNode = searchParentNode(node.data);
-  //     pNode[pos] = node.right;
-  //     node.right = null;
+    const leftHeight = height(leftNode.data);
+    const rightHeight = height(rightNode.data);
 
-  //     if (parentNode === undefined) {
-  //       const newNode = Node(grandChildNode.data, node.left, node.right);
-  //       treeRoot = newNode;
-  //     } else {
-  //       parentNode[pos] = grandChildNode;
-  //       grandChildNode.left = node.left;
-  //       grandChildNode.right = node.right;
-  //     }
-  //   }
-  // };
+    if (isBalancedRec(leftNode) === false) {
+      return false;
+    }
+    if (isBalancedRec(rightNode) === false) {
+      return false;
+    }
 
-  const toString = (tree = treeRoot) => {
+    if (leftHeight - rightHeight > 1 || rightHeight - leftHeight > 1) {
+      return false;
+    }
+    return true;
+  }
+
+  function isBalanced(tree = treeRoot) {
+    return isBalancedRec(tree);
+  }
+
+  function reBalance(tree = treeRoot) {
+    const arr = [];
+    inorder((e) => {
+      arr.push(e);
+    }, tree);
+    treeRoot = buildTree(arr);
+  }
+
+  function toString(tree = treeRoot) {
     prettyPrint(tree);
+  }
+
+  return {
+    find,
+    insert,
+    deleteNode,
+    levelOrder,
+    inorder,
+    preorder,
+    postorder,
+    height,
+    depth,
+    isBalanced,
+    reBalance,
+    toString,
   };
-
-  // prettyPrint(treeRoot);
-  // console.log(treeRoot);
-  return { find, insert, deleteNode, toString };
 };
-
-const array = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
-  23, 24, 25, 26, 27, 28, 29, 30, 31,
-];
-const trr = Tree(array);
-// trr.find(0);
-// trr.insert(3);
-// trr.insert(11);
-trr.deleteNode(16);
-trr.deleteNode(16);
-
-trr.deleteNode(22);
-trr.deleteNode(20);
-trr.deleteNode(28);
-trr.toString();
-
-trr.deleteNode(18);
-
-trr.toString();
